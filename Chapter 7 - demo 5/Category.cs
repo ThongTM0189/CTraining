ddfd620bbd182d9417b1f831910db75d8ec2e215
ThongTM0189 <89827492+congdeptrainhat@users.noreply.github.com>
2024-06-17 21:08:33 +0700

    update code chapter

---
+++ Chapter 7 - demo 5/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7___demo_5
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Chapter 7 - demo 5/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chapter_7___demo_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static List<Category> categories = new List<Category>
+        {
+            new Category{Id = 1, Name = "Cate 1"},
+            new Category{Id = 2, Name = "Cate 2"},
+            new Category{Id = 3, Name = "Cate 3"},
+            new Category{Id = 4, Name = "Cate 4"},
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbID.IsEnabled = false;
+            loadData();
+        }
+
+        private void loadData()
+        {
+            dbCategory.ItemsSource = null;
+            dbCategory.ItemsSource = categories;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string name = tbName.Text;
+            if (!Int32.TryParse(tbID.Text, out int id))
+            {
+                MessageBox.Show("ID must input number");
+                return;
+            }
+
+            bool check = categories.FirstOrDefault(x => x.Id == id) != null;
+            if (check)
+            {
+                MessageBox.Show("ID was existed");
+                return;
+            }
+
+            categories.Add(new Category { Id = id, Name = name });
+            loadData();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Int32.TryParse(tbID.Text, out int id))
+            {
+                MessageBox.Show("ID must input number");
+                return;
+            }
+
+            Category cate = categories.FirstOrDefault(x => x.Id == id);
+            if (cate == null)
+            {
+                MessageBox.Show("ID was not existed");
+                return;
+            }
+
+            categories.Remove(cate);
+            loadData();
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            string name = tbName.Text;
+            if (!Int32.TryParse(tbID.Text, out int id))
+            {
+                MessageBox.Show("ID must input number");
+                return;
+            }
+
+            Category cate = categories.FirstOrDefault(x => x.Id == id);
+            cate.Name = name;
+            categories.ForEach(x =>
+            {
+                if(x.Id == id)
+                {
+                    x.Name = name;
+                    return;
+                }
+            });
+            loadData();
+        }
+    }
+}
