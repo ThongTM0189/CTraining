f717dbea615e9737ccde400a7dfdd8685184539f
ThongTM0189 <89827492+congdeptrainhat@users.noreply.github.com>
2024-06-18 20:35:54 +0700

    update code

--- Chapter 7 - demo 5/MainWindow.xaml.cs
+++ Chapter 7 - demo 5/MainWindow.xaml.cs
@@ -18,22 +18,29 @@ namespace Chapter_7___demo_5
     {
         private static List<Category> categories = new List<Category>
         {
-            new Category{Id = 1, Name = "Cate 1"},
-            new Category{Id = 2, Name = "Cate 2"},
-            new Category{Id = 3, Name = "Cate 3"},
-            new Category{Id = 4, Name = "Cate 4"},
+            new Category{Id = 1, Name = "code 1"},
+            new Category{Id = 2, Name = "c++ 2"},
+            new Category{Id = 3, Name = "Java 3"},
+            new Category{Id = 4, Name = "JavaScript 4"},
         };
+
         public MainWindow()
         {
             InitializeComponent();
-            //tbID.IsEnabled = false;
             loadData();
         }
 
-        private void loadData()
+        private void loadData(List<Category> data = null)
         {
             dbCategory.ItemsSource = null;
-            dbCategory.ItemsSource = categories;
+            if(data == null)
+            {
+                dbCategory.ItemsSource = categories; // tương đương với dataContext
+            }
+            else
+            {
+                dbCategory.ItemsSource = data;
+            }
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
@@ -49,52 +56,73 @@ namespace Chapter_7___demo_5
                 categories.Add(new Category { Id = id, Name = name });
                 loadData();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-            
+
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
-            if (!Int32.TryParse(tbID.Text, out int id))
+            try
             {
-                MessageBox.Show("ID must input number");
-                return;
-            }
+                if (!Int32.TryParse(tbID.Text, out int id)) throw new Exception("ID must input number");
 
-            Category cate = categories.FirstOrDefault(x => x.Id == id);
-            if (cate == null)
+                var cate = categories.FirstOrDefault(x => x.Id == id);
+                if (cate == null) throw new Exception("ID was not existed");
+
+                categories.Remove(cate);
+                loadData();
+            }
+            catch (Exception ex)
             {
-                MessageBox.Show("ID was not existed");
-                return;
+                MessageBox.Show(ex.Message);
             }
-
-            categories.Remove(cate);
-            loadData();
         }
 
         private void btnUpdate_Click(object sender, RoutedEventArgs e)
         {
-            string name = tbName.Text;
-            if (!Int32.TryParse(tbID.Text, out int id))
+            try
             {
-                MessageBox.Show("ID must input number");
-                return;
-            }
+                string name = tbName.Text;
+                if (!Int32.TryParse(tbID.Text, out int id)) throw new Exception("ID must input number");
 
-            Category cate = categories.FirstOrDefault(x => x.Id == id);
-            cate.Name = name;
-            categories.ForEach(x =>
-            {
-                if(x.Id == id)
+                var check = categories.FirstOrDefault(x => x.Id == id) == null;
+                if (check) throw new Exception("ID was not existed");
+
+                categories.ForEach(x =>
                 {
-                    x.Name = name;
-                    return;
-                }
-            });
-            loadData();
+                    if (x.Id == id)
+                    {
+                        x.Name = name;
+                        return;
+                    }
+                });
+                loadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void tbID_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ban da click vao button");
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string txtSearch = tbSearch.Text.ToLower();
+            List<Category> searchCategory = new List<Category>();
+            searchCategory = categories.Where(cate => cate.Name.ToLower().Contains(txtSearch)).ToList();
+            loadData(searchCategory);
         }
     }
 }
